fefc3c36cd00656a9860e7815b44f8edaf6a247e
palmally <palmally@yandex.ru>
2023-03-08 15:02:41 +0300

    feat: add jest & classNames unit

---
+++ src/shared/lib/classNames/className.test.ts
@@ -0,0 +1,21 @@
+import { cn } from './classNames';
+
+describe('classNames', () => {
+  test('only first param', () => {
+    expect(cn('someClass')).toBe('someClass');
+  });
+  test('with additional (1 and 3 params)', () => {
+    expect(cn('someClass', {}, ['add1', 'add2'])).toBe('someClass add1 add2');
+  });
+  test('with mods and additional (all params)', () => {
+    const expected = 'someClass add1 add2 active visible';
+    expect(
+      cn('someClass', { active: true, visible: true, scrolled: false }, ['add1', 'add2']),
+    ).toBe(expected);
+  });
+  test('with strings in mods', () => {
+    expect(cn('someClass', { active: 'string', visible: '', scrolled: undefined })).toBe(
+      'someClass active',
+    );
+  });
+});
